48556584cd4f567f29ee4c65fbc1ce737c8ad669
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:33:18 +0500

    first commit

---
+++ Empty_array.js
@@ -0,0 +1,9 @@
+"use strict";
+//Think of something you could store in a array.
+let items = ['Pakistan', 'karachi', 'Urdu'];
+//Write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Paris');
+item2.push('Dubai');
+item2.push('England');
+console.log(item2);
---
+++ Empty_array.ts
@@ -0,0 +1,12 @@
+//Think of something you could store in a array.
+let items :string[] = ['Pakistan','karachi','Urdu'];
+
+//Write a program that creates a list containing these items.
+
+let item2 :string[] = [] ;
+
+item2.push('Paris');
+item2.push('Dubai');
+item2.push('England');
+
+console.log(item2);
